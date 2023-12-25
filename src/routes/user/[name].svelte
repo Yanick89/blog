669d0eebdbbea5c45b35daf581c4b1598dbb7fba
Yanick <moussoundayannick89@gmail.com>
2023-12-25 21:41:05 +0100

    add news component

--- src/routes/user/[name].svelte
+++ src/routes/user/[name].svelte
@@ -1,12 +1,6 @@
 
-
-
-<div class="container w-full max-w-3xl mx-auto">
-    <div class="px-4 sm:px-0">
-        <h3 class="text-base font-semibold leading-7 text-gray-900">Applicant Information</h3>
-        <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details and application.</p>
-    </div>
-    <div class="mt-6 border-t border-gray-100">
+<div class="container w-full  max-w-4xl mx-auto flex">
+    <div class="mt-6 border-t border-gray-100 space-y-12 w-full">
         <dl class="divide-y divide-gray-100">
         <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
             <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
@@ -64,6 +58,14 @@
             </dd>
         </div>
         </dl>
+    </div>
+    <div class="col-span-full bg-white border-l mx-auto h-screen">        
+        <div class="flex flex-col items-center gap-x-3">
+            <svg class="h-28 w-28 text-gray-300" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                <path fill-rule="evenodd" d="M18.685 19.097A9.723 9.723 0 0021.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 003.065 7.097A9.716 9.716 0 0012 21.75a9.716 9.716 0 006.685-2.653zm-12.54-1.285A7.486 7.486 0 0112 15a7.486 7.486 0 015.855 2.812A8.224 8.224 0 0112 20.25a8.224 8.224 0 01-5.855-2.438zM15.75 9a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" clip-rule="evenodd" />
+            </svg>
+            <a href="/setting/edit-profil" class="text-gray-500 bg-gray-200 hover:bg-gray-300 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Modifier</a>
+        </div>
     </div>
 </div>
   
